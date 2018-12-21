d21f6a2d6882d80d4a332d59bbb3f177aa5e291d
Pedro Moreira <pedsmoreira@gmail.com>
2018-12-21 00:31:29 -0200

    Add battle-casex eol configuration

--- src/index.js
+++ src/index.js
@@ -10,4 +10,9 @@ import Battlecry from './classes/Battlecry';
 import dd from './helpers/dd';
 import log from './helpers/log';
 
+import { EOL } from 'os';
+import { configure } from 'battle-casex';
+
+configure({ eol: EOL });
+
 export { ArgBuilder, File, Generator, GeneratorMethod, OptionBuilder, Battlecry, dd, log };
