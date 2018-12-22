1885972f462bb5480aa641f486807262c311a72f
Pedro Moreira <pedsmoreira@gmail.com>
2018-12-22 10:59:33 -0200

    Update jest.setup.js

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -1,4 +1,6 @@
 // Avoid console output
 jest.spyOn(global.console, 'log').mockImplementation(() => jest.fn());
 jest.spyOn(global.console, 'error').mockImplementation(() => jest.fn());
+
+// Commander.js calls process.stdout
 jest.spyOn(process.stdout, 'write').mockImplementation(() => jest.fn());
